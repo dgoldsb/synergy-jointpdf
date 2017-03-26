97d5c8a9fb0b2c37b701f88157776f89b4f775f9
dgoldsb <dgoldsb@live.nl>
2017-03-26 22:34:43 +0200

    Synergy profile working, envelope is next.

--- scripts/explore.py
+++ scripts/explore.py
@@ -11,20 +11,23 @@ def main():
     """
 
     # Create a single profile and plot it
-    # TODO: make it a class too, just a discrete and cont version
     system = MIprofile.create_system_discrete()
     plot_data = MIprofile.create_mi_profile(system)
     MIprofile.plot_mi_profile(plot_data)
 
     # Create several profiles and plot them
-    print("Todo...")
+    # First do a set of 50 systems that are unbiased
+    # TODO: make a set of the classes
+    # Now, do it for a set that are a function of each other
+    # TODO: do this too
 
-    # Do some stuff with a single system
+    # For testing: create a system, add a variable, 
     system = jointpdf.JointProbabilityMatrix(numvariables=2, numvalues=2
                                              , joint_probs='unbiased')
     print("The entropy of the system is "+str(system.entropy()))
     system.append_variables(num_added_variables=1, added_joint_probabilities=None)
     print("The entropy of the system with another variable appended is "+str(system.entropy()))
+    # TODO: verify that the added variable entropy is more than the difference
     system.append_synergistic_variables(num_synergistic_variables=1
                                         , initial_guess_summed_modulo=False
                                         , verbose=True, subject_variables=None, agnostic_about=None
