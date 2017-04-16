c1d4c633d922644bf78244672f8df516cac2ea5e
dgoldsb <dgoldsb@live.nl>
2017-04-16 19:25:22 +0200

    Made some progress, need to evolve the ODE system.

--- scripts/resilientpdf/resilientpdf.py
+++ scripts/resilientpdf/resilientpdf.py
@@ -8,6 +8,8 @@ It can be optimized to minimize nudge effect, and maximize memory.
 
 from __future__ import print_function
 import numpy as np
+from deap import creator, base, tools, algorithms
+from scipy.optimize import basinhopping
 import NPEET.entropy_estimators as entr
 
 __author__ = 'dgoldsb'
@@ -49,8 +51,18 @@ class System(object):
         Generates the ODE system.
         Is called as a part of the training.
         Should be done initially, it creates enough parameters and makes some guesses.
+        Basically centered around zero with some noise slapped on.
         """
+        # We throw the parameters in a single list
+        # Total size**2 for all component interactions
+        # Add size parameters for the constants
+        # The pattern is as follows:
+        # a, a>a, b>a, c>a, b, a>b, b>b, c>b, c etc.
+        num_parameters = self.size**2 + self.size
         self.ode_params = []
+        for _ in range(0, num_parameters):
+            # For now going for a set error distribution
+            self.ode_params.append(np.random.normal(0, 1))
         return 0
 
     def add_component(self, mean, stdev):
@@ -76,7 +88,7 @@ class System(object):
         """
         return state
 
-    def evolve_system(self, state, delta_t):
+    def evolve_system(self, state, delta_t, parameters):
         """
         Evolve the system, usually t = 0 after a nudge.
         """
@@ -84,7 +96,7 @@ class System(object):
         state_future = state
         return state_now, state_future
 
-    def cost(self):
+    def cost(self, parameters):
         """
         Generally is used to compare the unnudged state with the outcome.
         Done by calculating the MI between the unnudged state, and the final state.
@@ -92,11 +104,32 @@ class System(object):
         state_initial = self.sample_system(self.sample_size)
         state_nudged = self.nudge_system(state_initial, self.error_mean,
                                          self.error_sd, self.num_affected)
-        state_end = self.evolve_system(state_nudged, self.delta_t)
-        for component_initial in state_initial:
-            entr.ennt
+        state_end = self.evolve_system(state_nudged, self.delta_t, parameters)
 
         # Cost is the average difference between the entropy of the outcome
         # and the MI between the unnudged start and the end-state
-        cost = 0
+        costs = []
+        for component_initial, component_end in state_initial, state_end:
+            entropy = entr.entropy(x=component_end)
+            mutual_info = entr.mi(x=component_initial, y=component_end)
+            costs.append(entropy-mutual_info)
+        cost = np.average(costs)
+
         return cost
+
+    def train(self, method):
+        """
+        Train the parameters using the deap library (genetic algorithm) or scipy (annealing)
+        """
+        # Start with the ODE params
+        # Check if there is a previous result, if not do a guess
+        if method == 'evolutionary':
+            # Add noise to guess to create a population
+            # TODO: follow the example of https://pypi.python.org/pypi/deap
+            outcome = []
+            self.ode_params = outcome
+        if method == 'annealing':
+            func = self.cost
+            guess = self.ode_params
+            self.ode_params = basinhopping(func, guess)
+        return 0
