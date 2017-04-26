Finished plotting, fixed KL divergence, improved logger

scripts/resilientpdf/resilientpdf.py
@@ -9,19 +9,36 @@ It can be optimized to minimize nudge effect, and maximize memory.
 from __future__ import print_function
 import sys
 import random
+import math
 import os
+import copy
 import logging
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 from scipy import optimize
 from scipy.integrate import ode as integrator
 import NPEET.entropy_estimators as npeet
-__author__ = 'dgoldsb'
 
+
+__author__ = 'dgoldsb'
+sns.set(color_codes=True)
 ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..')
-logging.basicConfig(filename=os.path.join(ROOT, 'log/resilientpdf.log'),
-                    level=logging.INFO, format='%(asctime)s '+\
-                    '[%(levelname)s] %(message)s', filemode='w')
-logging.getLogger().addHandler(logging.StreamHandler())
+
+
+mylogger = logging.getLogger('mylogger')
+handler1 = logging.FileHandler(filename=os.path.join(ROOT, 'log/resilientpdf.log'),\
+                               mode='w')
+handler1.setLevel(logging.DEBUG)
+handler1.setFormatter(logging.Formatter('%(asctime)s '+\
+                    '[%(levelname)s] %(message)s'))
+mylogger.addHandler(handler1)
+handler2 = logging.StreamHandler(sys.stdout)
+handler2.setLevel(logging.INFO)
+handler2.setFormatter(logging.Formatter('%(asctime)s '+\
+                    '[%(levelname)s] %(message)s'))
+mylogger.addHandler(handler2)
+mylogger.setLevel(logging.DEBUG)
 
 class Component(object):
     """
@@ -46,7 +63,7 @@ class System(object):
     """
 
     def __init__(self, sample_size=1000, error_mean=0, error_sd=1,
-                 num_nudged=1, delta_t=1, self_loop=False):
+                 num_nudged=1, delta_t=1, self_loop=False, visualmode=1):
         self.size = 0
         self.components = []
         self.ode_params = []
@@ -59,6 +76,85 @@ class System(object):
         self.current_sample = None
         self.state_nudged = None
         self.self_loop = self_loop
+        self.visualmode = visualmode # 0 nothing, 1 before/after, 2 every training iter
+
+    def plot_ode(self):
+        """
+        Plots the ODE system over time from the current initial values.
+        Uses current parameter setup.
+        Supports 3D plot for 3-dimensional system.
+        """
+        if self.size == 3:
+            return 0
+        else:
+            return 0
+
+    def plot_kld(self, set_x, set_xp):
+        """
+        Plots the two distributions over each other,
+        Also adds the Kullback-Leibler divergence.
+        """
+        no_per_row = 4
+        if self.size < 4:
+            no_per_row = self.size
+        set_x_t = np.asarray(set_x).T.tolist()
+        set_xp_t = np.asarray(set_xp).T.tolist()
+        kld = npeet.kldiv(x=set_x_t, xp=set_xp_t, k=10)
+        rows = int(math.ceil(float(self.size) / no_per_row))
+        fig, axs = plt.subplots(rows, no_per_row, figsize=(10, 10), facecolor='w', edgecolor='k')
+        fig.suptitle('Comparisons with KL-divergence = '+str(kld), fontsize=16)
+        fig.subplots_adjust(hspace=.5, wspace=.2)
+        axs = axs.ravel()
+        for i in range(0, self.size):
+            axs[i].hist([set_x[i], set_xp[i]], color=['r', 'b'], alpha=0.5, normed=True)
+            axs[i].set_ylim(0, 1)
+            axs[i].set_title(str(i+1))
+        plt.show()
+        return 0
+
+    def plot_mi(self, set_x, set_y):
+        """
+        Plots the two distributions over each other,
+        Also adds the mutual information.
+        """
+        no_per_row = 4
+        if self.size < 4:
+            no_per_row = self.size
+        set_x_t = np.asarray(set_x).T.tolist()
+        set_y_t = np.asarray(set_y).T.tolist()
+        mutualinfo = npeet.mi(x=set_x_t, y=set_y_t, k=10)
+        rows = int(math.ceil(float(self.size) / no_per_row))
+        print(rows)
+        fig, axs = plt.subplots(rows, no_per_row, figsize=(10, 10), facecolor='w', edgecolor='k')
+        fig.suptitle('Comparisons with mutual information = '+str(mutualinfo), fontsize=16)
+        fig.subplots_adjust(hspace=.5, wspace=.2)
+        axs = axs.ravel()
+        for i in range(0, self.size):
+            axs[i].hist([set_x[i], set_y[i]], color=['r', 'b'], alpha=0.5, normed=True)
+            axs[i].set_ylim(0, 1)
+            axs[i].set_title(str(i+1))
+        plt.show()
+        return 0
+
+    def plot_pdfs(self, sets, title=None):
+        """
+        Plots a set of samples, drawn from PDFs.
+        """
+        no_per_row = 4
+        if self.size < 4:
+            no_per_row = self.size
+        rows = int(math.ceil(float(len(sets)) / no_per_row))
+        fig, axs = plt.subplots(rows, no_per_row, figsize=(10, 10), facecolor='w', edgecolor='k')
+        if title is not None:
+            fig.suptitle(title, fontsize=16)
+        fig.subplots_adjust(hspace=.5, wspace=.2)
+        axs = axs.ravel()
+        i = 0
+        for set_plot in sets:
+            axs[i].hist(set_plot, alpha=0.5)
+            axs[i].set_title(str(i+1))
+            i = i + 1
+        return 0
 
     def generate_ode_params(self):
         """
@@ -80,7 +176,7 @@ class System(object):
         self.ode_params = []
         for _ in range(0, num_parameters):
             self.ode_params.append(np.random.normal(0, 1))
-        logging.info("Making an initial guess for the ODE parameter vector: "\
+        mylogger.info("Making an initial guess for the ODE parameter vector: "\
                      +str(self.ode_params))
         return 0
 
@@ -90,7 +186,7 @@ class System(object):
         """
         self.components.append(Component(mean, stdev))
         self.size = len(self.components)
-        logging.info("Added component: mean "+str(mean)+" and SD "+str(stdev))
+        mylogger.info("Added component: mean "+str(mean)+" and SD "+str(stdev))
         return 0
 
     def sample_system(self, sample_size):
@@ -100,7 +196,7 @@ class System(object):
         state = []
         for component in self.components:
             state.append(component.sample(sample_size))
-        logging.info("Drawn a sample of size "+str(sample_size)+" from the system")
+        mylogger.info("Drawn a sample of size "+str(sample_size)+" from the system")
         return state
 
     def nudge_system(self, state, error_mean, error_sd):
@@ -109,20 +205,21 @@ class System(object):
         Each sample is nudged with the same draw from the same random normal distribution.
         This shifts the mean of the distribution the samples are drawn from
         """
-        state_nudged = state[:]
+        state_nudged = copy.deepcopy(state)
         nudges_left = self.num_nudged
         nudge_candidates = [i for i in range(0, self.size)]
         while len(nudge_candidates) > 0 and nudges_left > 0:
             # Do a nudge, addition of a noise normal distribution, effectively increases sigma
             target = random.choice(nudge_candidates)
-            nudge_candidates = np.delete(nudge_candidates, target)
+            nudge_candidates = np.delete(nudge_candidates, list(nudge_candidates).index(target))
             nudges_left = nudges_left - 1
             for i in range(0, len(state_nudged[target])):
                 state_nudged[target][i] = state_nudged[target][i] +\
                                           np.random.normal(error_mean, error_sd)
-        logging.info("Nudged the initial sample (avg before/after):")
-        logging.info([np.average(a) for a in state])
-        logging.info([np.average(a) for a in state_nudged])
+
+        mylogger.debug("Nudged the initial sample (avg before/after):")
+        mylogger.debug([np.average(a) for a in state])
+        mylogger.debug([np.average(a) for a in state_nudged])
         return state_nudged
 
     def f_linear(self, t, ys, parameters):
@@ -145,7 +242,7 @@ class System(object):
                 end = (self.size) + (self.size) * i
                 a = parameters[start:end]
                 dy_dt = a[0]
-                ys_popped = ys[:]
+                ys_popped = copy.deepcopy(ys)
                 ys_popped = np.delete(ys_popped, i)
                 for j in range(0, self.size-1):
                     dy_dt = dy_dt + a[j+1] * ys_popped[j]
@@ -158,7 +255,7 @@ class System(object):
         """
         # Transpose the thing to get a list of components per sample, instead of
         # samples per component
-        state_now = state[:]
+        state_now = state
         state_now_t = np.asarray(state_now).T.tolist()
 
         # Create an empty list to fill with future components per sample
@@ -174,11 +271,11 @@ class System(object):
             state_future_t.append(sample_future)
 
         state_future = np.asarray(state_future_t).T.tolist()
-        logging.info("Evolved the system from t = 0 to t = "+str(delta_t)+" (avg before/end)")
-        logging.info([np.average(a) for a in state_now])
-        logging.info([np.average(a) for a in state_future])
-        logging.info("The parameters used are:")
-        logging.info(parameters)
+        mylogger.debug("Evolved the system from t = 0 to t = "+str(delta_t)+" (avg before/end)")
+        mylogger.debug([np.average(a) for a in state_now])
+        mylogger.debug([np.average(a) for a in state_future])
+        mylogger.debug("The parameters used are:")
+        mylogger.debug(parameters)
         return state_now, state_future
 
     def cost(self, parameters):
@@ -186,27 +283,55 @@ class System(object):
         Generally is used to compare the unnudged state with the outcome.
         Done by calculating the MI between the unnudged state, and the final state.
         """
-        _, state_end_nudged = self.evolve_system(self.state_nudged[:], self.delta_t, parameters)
-        _, state_end_unnudged = self.evolve_system(self.current_sample[:], self.delta_t, parameters)
+        if self.visualmode == 2:
+            title = 'Comparison startstates nudged vs. unnudged'
+            sets = []
+            for i in range(0, self.size):
+                sets.append([self.state_nudged[i], self.current_sample[i]])
+            self.plot_pdfs(sets, title)
+        _, state_end_nudged = self.evolve_system(copy.deepcopy(self.state_nudged), self.delta_t, parameters)
+        _, state_end_unnudged = self.evolve_system(copy.deepcopy(self.current_sample), self.delta_t, parameters)
+        if self.visualmode == 2:
+            title = 'Comparison endstates nudged vs. unnudged'
+            sets = []
+            for i in range(0, self.size):
+                sets.append([state_end_nudged[i], state_end_unnudged[i]])
+            self.plot_pdfs(sets, title)
 
-        # Cost is the average difference between the entropy of the outcome
-        # and the MI between the unnudged start and the end-state
-        costs = []
-        for i in range(0, self.size):
-            try:
-                nudged_set = [[j] for j in state_end_nudged[i]]
-                unnudged_set = [[j] for j in state_end_unnudged[i]]
-                mutual_info = npeet.mi(x=nudged_set, y=unnudged_set, k=10)
-            except AssertionError as a:
-                logging.critical('NPEET failed: '+str(a))
-                sys.exit(1)
+        # Use NPEET to find the mutual information
+        # This is used to measure memory
+        try:
+            if self.visualmode == 2:
+                self.plot_mi(self.current_sample, state_end_unnudged)
+            start_set = np.asarray(self.current_sample).T.tolist()
+            end_set = np.asarray(state_end_unnudged).T.tolist()
+            mutual_info = npeet.mi(x=start_set, y=end_set, k=10)
             if mutual_info < 0:
                 mutual_info = 1e-15
-            logging.info('Mutual information for '+str(i)+': '+str(mutual_info))
-            costs.append(1/mutual_info)
-        cost = np.max(costs)
+                mylogger.error('MI below zero encountered')
+        except AssertionError as error:
+            mylogger.critical('NPEET failed: '+str(error))
+            sys.exit(1)
+        mylogger.info('Mutual information for : '+str(mutual_info))
+
+        # Use NPEET to find the Kullbar-Leibler divergence
+        try:
+            if self.visualmode == 2:
+                self.plot_kld(state_end_nudged, state_end_unnudged)
+            nudged_set = np.asarray(state_end_nudged).T.tolist()
+            unnudged_set = np.asarray(state_end_unnudged).T.tolist()
+            kl_div = npeet.kldiv(x=nudged_set, xp=unnudged_set, k=10)
+            if kl_div < 0:
+                kl_div = 0
+                mylogger.error('KL-div below zero encountered')
+        except AssertionError as error:
+            mylogger.critical('NPEET failed: '+str(error))
+            sys.exit(1)
+        mylogger.info('KL divergence: '+str(kl_div))
+
+        cost = - mutual_info + kl_div
         self.iteration = self.iteration + 1
-        logging.info('Cost at iteration '+str(self.iteration)+' is '+str(cost))
+        mylogger.info('Cost at iteration '+str(self.iteration)+' is '+str(cost))
         return cost
 
     def train(self, method='minimize', cycles=10):
@@ -223,25 +348,25 @@ class System(object):
         bounds = [[-2, 2] for _ in range(0, len(self.ode_params))]
 
         for i in range(0, cycles):
-            logging.info("Starting cycle "+str(i+1)+" of "+str(cycles))
+            mylogger.info("Starting cycle "+str(i+1)+" of "+str(cycles))
             self.iteration = 1
             self.current_sample = self.sample_system(self.sample_size)
-            self.state_nudged = self.nudge_system(self.current_sample[:],
+            self.state_nudged = self.nudge_system(copy.deepcopy(self.current_sample),
                                                   self.error_mean, self.error_sd)
             if method == 'evolutionary':
-                logging.info("Training the ODE pars using scipy.optimize.differential_evolution")
+                mylogger.info("Training the ODE pars using scipy.optimize.differential_evolution")
                 self.ode_params = optimize.differential_evolution(func=func, bounds=bounds)
             elif method == 'minimize':
-                logging.info("Training the ODE pars using scipy.optimize.minimize")
+                mylogger.info("Training the ODE pars using scipy.optimize.minimize")
                 guess = self.ode_params
                 self.ode_params = optimize.minimize(fun=func, x0=guess, bounds=bounds
                                                     , method="Nelder-Mead")
             elif method == 'basinhopping':
-                logging.info("Training the ODE pars using scipy.optimize.basinhopping")
+                mylogger.info("Training the ODE pars using scipy.optimize.basinhopping")
                 guess = self.ode_params
                 self.ode_params = optimize.basinhopping(func=func, x0=guess)
             else:
-                logging.error("No optimizer selected, exiting...")
+                mylogger.error("No optimizer selected, exiting...")
                 sys.exit(1)
         return 0
 
@@ -249,11 +374,9 @@ def main():
     """
     For basic testing purposes.
     """
-    system = System(num_nudged=1)
+    system = System(num_nudged=0, error_mean=0, error_sd=1, visualmode=1)
     system.add_component(10, 1)
-    system.add_component(5, 1)
-    system.add_component(3, 1)
-    system.train(method='minimize', cycles=1)
+    system.train(method='evolutionary', cycles=1)
     print(system.ode_params)
 
 if __name__ == '__main__':