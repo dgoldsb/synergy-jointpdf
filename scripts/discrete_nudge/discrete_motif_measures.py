Finished complete experimental design, to be improved, also fixed memory calculation

scripts/discrete_nudge/discrete_motif_measures.py
@@ -11,6 +11,7 @@ from scipy.linalg import norm
 from scipy.optimize import minimize
 from scipy.stats import entropy
 
+
 def total_correlation(motif, indices):
     """
     Returns the total correlation.
@@ -28,6 +29,7 @@ def total_correlation(motif, indices):
     for index in indices:
         return_value += motif.entropy(index)
     return return_value
+
 
 def abs_diff(tree_1, tree_2):
     """
@@ -56,6 +58,7 @@ def abs_diff(tree_1, tree_2):
 
     return returnval
 
+
 def hellinger(tree_1, tree_2):
     """
     Finds the absolute difference between two same-shaped FullNestedArrayOfProbabilities objects.
@@ -79,6 +82,7 @@ def hellinger(tree_1, tree_2):
 
     return norm(np.sqrt(t1_flat) - np.sqrt(t2_flat)) / np.sqrt(2)
 
+
 def kl_div(tree_1, tree_2):
     """
     Finds the KL-divergence for the motif after a time evaluation.
@@ -97,6 +101,7 @@ def kl_div(tree_1, tree_2):
 
     return entropy(t1_flat, t2_flat)
 
+
 def mutual_information(motif, genes_t0 = None, genes_t1 = None):
     """
     Finds the mutual information for the motif after a time evaluation.
@@ -110,7 +115,7 @@ def mutual_information(motif, genes_t0 = None, genes_t1 = None):
     returnval: mutual information (float)
     """
     if motif.grn_vars["gene_cnt"] == motif.numvariables:
-        raise ValueError("do a time evaluation before attemting to check MI")
+        raise ValueError("do a time evaluation before attempting to check MI")
 
     # define what t0 and t1 are
     if genes_t0 is None:
@@ -119,6 +124,7 @@ def mutual_information(motif, genes_t0 = None, genes_t1 = None):
         genes_t1 = range(motif.grn_vars["gene_cnt"], motif.numvariables)
 
     return motif.mutual_information(genes_t0, genes_t1)
+
 
 def synergy_quax(motif, tolerance=0.2):
     """
@@ -133,7 +139,7 @@ def synergy_quax(motif, tolerance=0.2):
     returnval: upper bound of synergy according to Rick Quax (float)
     """
     if motif.grn_vars["gene_cnt"] == motif.numvariables:
-        raise ValueError("do a time evaluation before attemting to check synergy")
+        raise ValueError("do a time evaluation before attempting to check synergy")
 
     # define what t0 and t1 are
     genes_t0 = range(0, motif.grn_vars["gene_cnt"])
@@ -142,6 +148,7 @@ def synergy_quax(motif, tolerance=0.2):
     return motif.synergistic_information(genes_t1, genes_t0, tol_nonsyn_mi_frac=tolerance, verbose=False,
                                          minimize_method=None, num_repeats_per_srv_append=30,
                                          initial_guess_summed_modulo=False)
+
 
 def synergy_uii(motif, tolerance=0.2):
     """
@@ -156,7 +163,7 @@ def synergy_uii(motif, tolerance=0.2):
     returnval: uii according to Rick Quax (float)
     """
     if motif.grn_vars["gene_cnt"] == motif.numvariables:
-        raise ValueError("do a time evaluation before attemting to check synergy")
+        raise ValueError("do a time evaluation before attempting to check synergy")
 
     # define what t0 and t1 are
     genes_t0 = range(0, motif.grn_vars["gene_cnt"])
@@ -178,7 +185,7 @@ def synergy_wms(motif):
     returnval: WMS synergy (float)
     """
     if motif.grn_vars["gene_cnt"] == motif.numvariables:
-        raise ValueError("do a time evaluation before attemting to check synergy")
+        raise ValueError("do a time evaluation before attempting to check synergy")
 
     # define what t0 and t1 are
     genes_t0 = range(0, motif.grn_vars["gene_cnt"])
@@ -186,22 +193,23 @@ def synergy_wms(motif):
 
     return motif.synergistic_information_naive(genes_t1, genes_t0)
 
+
 def synergy_middleground(motif):
     """
     A synergy approximation by taking the mean of the upper bound (single MI with whole system)
     and lower bound (WMS)
-    
+
     PARAMETERS
     ---
     motif: a DiscreteGrnMotif
-    
+
     RETURNS
     ---
     returnval: middle ground synergy approximation (float)
     """
     if motif.grn_vars["gene_cnt"] == motif.numvariables:
-        raise ValueError("do a time evaluation before attemting to check synergy")
-    
+        raise ValueError("do a time evaluation before attempting to check synergy")
+
     uppers = []
     genes = list(range(0, motif.grn_vars["gene_cnt"]))
     for _gene in genes:
@@ -209,6 +217,7 @@ def synergy_middleground(motif):
     upper = max(uppers)
     lower = synergy_wms(motif)
     return (upper + lower) / 2
+
 
 def mi_decay(motif, no_steps=8):
     """
@@ -224,12 +233,12 @@ def mi_decay(motif, no_steps=8):
     """
     memory = [motif.entropy()]
 
-    for i in range(0, no_steps):
+    for i in range(1, no_steps + 1):
         motif.evaluate_motif(cleanup_first=False)
         first = range(0, motif.grn_vars["gene_cnt"])
-        #print(motif.marginalize_distribution(first).joint_probabilities.joint_probabilities)
-        current = range(min(i, 2)*motif.grn_vars["gene_cnt"], motif.numvariables)
-        #print(motif.marginalize_distribution(current).joint_probabilities.joint_probabilities)
-        #print(motif.mutual_information(first, current))
+        current = range(i*motif.grn_vars["gene_cnt"], motif.numvariables)
         memory.append(motif.mutual_information(first, current))
+
+    # reset state
+    motif.reset_to_state(0)
     return memory

scripts/discrete_nudge/discrete_motif_operations.py
@@ -5,20 +5,24 @@ This file contains some of the common operations used on discrete motifs.
 from random import shuffle
 import numpy as np
 
+# TODO: extend the class more?
+
+
 def select_subset(arr, threshold):
     """
-    Import from Derkjan that broke down, so manually implementing
+    Import from Derkjan that broke down, so manually implementing.
 
     Select a subset of the arr (randomly) which is at least bigger than
-    threshold
+    threshold. From these parts of the array we subtract probability as part
+    of our nudge.
 
-    Parameters:
-    ----------
-    arr: a 1-d numpy array
+    :parameter
+    :param arr: a 1-d numpy array
         All entries should be greater or equal to zero
+    :param threshold: floating point number
 
-    Returns: a 1-d numpy filled with zeros and ones
-
+    :returns
+    :return minus_states: a 1-d numpy filled with zeros and ones
     """
     minus_states = np.random.randint(0, 2, arr.shape[0])
     attempt_count = 0
@@ -32,28 +36,25 @@ def select_subset(arr, threshold):
         indices = list(range(arr.shape[0]))
         np.random.shuffle(indices)
         for index in indices:
-            print(minus_states)
-            print(index)
-            print(arr)
-            if arr[minus_states] - arr[index] > threshold:
-                minus_states[index] = 0 # was selected, which does not exist, I hope this is correct
+            if np.sum(arr[minus_states != 0]) - arr[index] > threshold:
+                # was selected, which does not exist, I hope this is correct
+                minus_states[index] = 0
 
     return minus_states
 
 
 def mutate_array_bigger_zero(arr, nudge_size, option):
     """
-    Import from Derkjan that broke down, so manually implementing
+    Import from Derkjan that broke down, so manually implementing.
 
     Mutate the arr under the constraint that every entry must be
     bigger or equal to zero. The total plus and minus change should
-    both be equal to nudge size
+    both be equal to nudge size.
 
-    Parameters:
-    ----------
-    arr: a 1-d nd array
-    nudge_size: A (small) number
-    option: string in set: {proportional, random}
+    :parameter
+    :param arr: a 1-d np array
+    :param nudge_size: A (small) number
+    :param option: string in set: {proportional, random}
         how to create the vector to perform the nudge. With proportional
         the plus and minus states are chosen randomly (within the
         constraint that the minus states must have weight bigger than
@@ -64,7 +65,8 @@ def mutate_array_bigger_zero(arr, nudge_size, option):
         that part of the nudge is redistributed among the other states (
         again using Dirichlet)
 
-    Returns: a 1-d numpy array
+    :returns
+    :return nudged_array: a 1-d numpy array
 
     """
     nudged_array = np.copy(arr)
@@ -87,9 +89,9 @@ def mutate_array_bigger_zero(arr, nudge_size, option):
         minus_nudge = np.minimum(nudged_array[minus_mask], minus_nudge)
         difference = abs(np.sum(minus_nudge)-nudge_size)
         count = 0
-        while difference > 10**(-10) and count<10:
+        while difference > 10**(-10) and count < 10:
             count += 1
-            number_of_free_states = minus_nudge[nudged_array[minus_mask]!=minus_nudge].shape[0]
+            number_of_free_states = minus_nudge[nudged_array[minus_mask] != minus_nudge].shape[0]
             redistribute = difference * np.random.dirichlet([1]*number_of_free_states)
             minus_nudge[nudged_array[minus_mask] != minus_nudge] += redistribute
             minus_nudge = np.minimum(nudged_array[minus_mask], minus_nudge)
@@ -115,7 +117,7 @@ def mutate_array_bigger_zero(arr, nudge_size, option):
 
 def nudge_distribution_non_local_non_causal(joint, nudge_labels, nudge_size, nudge_option='random'):
     """
-    Import from Derkjan that broke down, so manually implementing
+    Import from Derkjan that broke down, so manually implementing.
 
     Nudge the the variable with nudge label while keeping the
     marginal of the other variables constant. Thus assuming that the variable
@@ -123,18 +125,17 @@ def nudge_distribution_non_local_non_causal(joint, nudge_labels, nudge_size, nud
     The nudge moves weight around in a random manner. Meaning that the weight
     does not go from one state to another state, but rather a random
     perturbation vector is placed on the states, its sum being equal
-    to 0 and its absolute sum equal to 2*nudge_size
+    to 0 and its absolute sum equal to 2*nudge_size.
 
-    Parameters:
-    ----------
-    joint: a numpy array
-        Representing a discrete probability distribution
-    nudge_labels: a list of integers
-    nudge_size: a (small) number
-    nudge_option: string in set {random, proportional}
+    :parameter
+    :param joint: a numpy array representing a discrete probability distribution
+    :param nudge_labels: a list of integers
+    :param nudge_size: a (small) floating point number
+    :param nudge_option: string in set {random, proportional}
         see mutate_array_bigger_zero option docs
 
-    Returns: a numpy array, representing the nudged probability distribution
+    :returns
+    :return nudged_joint: a nudged version of the joint
     """
     nudged_joint = np.copy(joint)
     nudged_indices = tuple(range(len(joint.shape) - len(nudge_labels), len(joint.shape), 1))
@@ -158,21 +159,23 @@ def nudge_distribution_non_local_non_causal(joint, nudge_labels, nudge_size, nud
     return nudged_joint
 
 
-def nudge_variable(motif, no_impacted, size=0.1):
+def nudge_variable(motif, targets, nudge_size, source):
     """
-    Nudge a variable or a number of variables, using DJ his method.
+    Nudge a variable or a number of variables, using DJ his method or the jointpdf method.
 
-    PARAMETERS
-    ---
-    motif: a DiscreteGrnMotif object
-    no_impacted: number of variables impacted
-    size: size of the nudge
+    :parameter
+    :param motif: a motif object
+    :param targets: the target variables to nudge
+    :param nudge_size: the total size of the nudge
+    :param source: the method to use, either jointpdf (jointpdf package) or DJ (modified implementation of
+        Derkjan his method, included here)
     """
-    joint = motif.joint_probabilities.joint_probabilities
-    labels = []
-    possible_labels = range(0, motif.numvariables)
-    for _ in range(0, no_impacted):
-        shuffle(possible_labels)
-        labels.append(possible_labels.pop())
-    new_joint = nudge_distribution_non_local_non_causal(joint, labels, size)
-    motif.joint_probabilities.joint_probabilities = new_joint
+
+    if source == 'joint_pdf':
+        motif.nudge(targets, [], nudge_size)
+    elif source == 'DJ':
+        joint = motif.joint_probabilities.joint_probabilities
+        new_joint = nudge_distribution_non_local_non_causal(joint, targets, nudge_size)
+        motif.joint_probabilities.joint_probabilities = new_joint
+    else:
+        raise ValueError("Source not correctly entered")

scripts/discrete_nudge/discrete_motif_plotting.py
@@ -6,10 +6,13 @@ and analysis on profiles.
 from __future__ import print_function
 
 import itertools
+import math
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import os
 import random
+import scipy.stats
 import string
 import time
 
@@ -17,6 +20,47 @@ import discrete_motif_measures as measures
 import discrete_motif_operations as operations
 
 __author__ = 'dgoldsb'
+
+
+def plot_line(values, colors, labels, title, filename=None, axes_labels=None):
+    """
+    example: http://alexanderfabisch.github.io/t-sne-in-scikit-learn.html
+
+    :param values: numpy array, should be a list containing [x-value, [y-values]]
+    :param colors: the color of each point
+    :param labels: the label of each point
+    :param title: the plot title
+    :param filename: the file to save to
+    :param axes_labels: labels for the axes
+    """
+    plt.title(title)
+    if axes_labels is not None:
+        plt.xlabel(axes_labels[0])
+        plt.ylabel(axes_labels[1])
+    for current_color in set(colors):
+        x_values = []
+        means = []
+        confidence_intervals = []
+        col_plot = []
+        lab_plot = []
+        for i in range(0, len(colors)):
+            if colors[i] == current_color:
+                # TODO: do a normality test, if it fails return without plotting
+                x_values.append(values[i][0])
+                n, min_max, mean, var, skew, kurt = scipy.stats.describe(values[i][1])
+                std = math.sqrt(var)
+                confidence_interval = scipy.stats.norm.interval(0.95, loc=mean, scale=std)
+                means.append(mean)
+                width_interval = confidence_interval[1] - mean
+                confidence_intervals.append(width_interval)
+                col_plot.append(colors[i])
+                lab_plot.append(labels[i])
+        plt.plot(x_values, means, c=col_plot[0], label=lab_plot[0])
+        plt.errorbar(x_values, means, yerr=confidence_intervals, fmt='x')
+    plt.legend(loc='upper left', numpoints=1, ncol=3, fontsize=8)
+    if filename is not None:
+        plt.savefig(filename, format='pdf')
+    plt.show()
 
 
 def plot_scatter(x_values, colors, labels, title, filename=None, axes_labels=None):
@@ -28,6 +72,7 @@ def plot_scatter(x_values, colors, labels, title, filename=None, axes_labels=Non
     :param labels: the label of each point
     :param title: the plot title
     :param filename: the file to save to
+    :param axes_labels: labels for the axes
     """
     plt.title(title)
     if axes_labels is not None:
@@ -46,10 +91,46 @@ def plot_scatter(x_values, colors, labels, title, filename=None, axes_labels=Non
         plt.scatter(x_plot[:, 0], x_plot[:, 1], c=col_plot[0], label=lab_plot[0], marker="x")
     plt.legend(loc='upper left', numpoints=1, ncol=3, fontsize=8)
     if filename is not None:
-        plt.savefig('myfig.pdf', format='pdf')
+        plt.savefig(filename, format='pdf')
     plt.show()
 
 
+def plot_scatter_3d(x_values, colors, labels, title, filename=None, axes_labels=None):
+    """
+    example: http://alexanderfabisch.github.io/t-sne-in-scikit-learn.html
+
+    :param x_values: numpy array
+    :param colors: the color of each point
+    :param labels: the label of each point
+    :param title: the plot title
+    :param filename: the file to save to
+    :param axes_labels: labels for the axes
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    plt.title(title)
+    if axes_labels is not None:
+        ax.set_xlabel(axes_labels[0])
+        ax.set_ylabel(axes_labels[1])
+        ax.set_zlabel(axes_labels[2])
+    for current_color in set(colors):
+        x_plot = []
+        col_plot = []
+        lab_plot = []
+        for i in range(0, len(colors)):
+            if colors[i] == current_color:
+                x_plot.append(x_values[i])
+                col_plot.append(colors[i])
+                lab_plot.append(labels[i])
+        x_plot = np.array(x_plot)
+        ax.scatter(x_plot[:, 0], x_plot[:, 1], c=col_plot[0], label=lab_plot[0], marker="x")
+    plt.legend(loc='lower right', numpoints=1, ncol=3, fontsize=8)
+    if filename is not None:
+        plt.savefig(filename, format='pdf')
+    plt.show()
+
+
+# TODO: below obsolete?
 def state_transition_table(motif, rule):
     """
     Find the state transition table, and return it as a list of list.