57b6808b8ee17705c3d6466aeee598a048be3d45
dgoldsb <dgoldsb@live.nl>
2018-01-02 12:37:35 +0100

    Finished experiments in pure Python, merging plots with the table generator

--- scripts/discrete_nudge/experiments_pandas.py
+++ scripts/discrete_nudge/experiments_pandas.py
@@ -17,6 +17,7 @@ import pandas as pd
 import pickle
 import random
 import scipy.stats
+import shutil
 from sklearn.manifold import TSNE
 import sys
 import tabulate
@@ -26,217 +27,15 @@ from time import gmtime, strftime
 # settings for the experiment
 synergy_measure = measures.synergy_middleground
 nudge_method = "DJ"
-sample_size = 200
-network_sizes = [2, 3, 4, 5]
-logic_sizes = [2, 3, 4]
+sample_size = 2 # in practice this is times two
+network_sizes = [2]
+logic_sizes = [2, 3]
 nudge_sizes = [0.1, 0.25, 0.5]
 
-
-def visualize_TSNE(dataframe):
-    """
-    We use t-SNE to visualize our sample. What we see makes perfect sense: the biological motifs seem to be a subset of the sample space. In higher valued logic systems, the space becomes larger, making the sample size insufficient. As t-SNE also does clustering, it then starts to appear that we have two seperate sample, but this is simply the separation from the very similar biological motifs from the rest.
-    """
-
-    # x_train should be an array of N vectors of length M
-    # where N is the sample size and M is the length of the combined new state of the transition table
-    # we combine both the random and bio samples, but give them different colors
-
-    for i in range(0, len(samples)):
-        x_train = []
-        y_color = []
-        labels = []
-        # the random tables
-        for sample in samples[i]["random_tables"]:
-            table = sample[0]
-            sample_vector = []
-            for row in table.transition_table:
-                sample_vector.extend(row[(len(row)/2):])
-            y_color.append("red")
-            x_train.append(sample_vector)
-            labels.append("Random transition table")
-
-        # the bio table
-        for sample in samples[i]["bio_tables"]:
-            motif = sample[0]
-            sample_vector = []
-            for row in motif.transition_table:
-                sample_vector.extend(row[(len(row)/2):])
-            y_color.append("blue")
-            x_train.append(sample_vector)
-            labels.append("Biological transition table")
-    
-        x_train = np.array(x_train)
-        title = "Two-dimensional embedding of transition tables with motif size %s and %s-valued logic" % (samples[i]["network_size"], samples[i]["logic_size"])
-        x_train_embedded = TSNE(n_components=2, perplexity=10, early_exaggeration=1, verbose=0).fit_transform(x_train)
-        
-        results_location = "../../data/n=%d_l=%d_e=%f_%s_%s_nosamples=%d" % (samples[i]["network_size"], samples[i]["logic_size"], samples[i]["nudge_size"], nudge_method, synergy_measure, len(samples[i]))
-        if not os.path.isdir(results_location):
-            os.mkdir(results_location)
-        filename = "tsne2D.pdf" 
-        visualize.plot_scatter(x_train_embedded, y_color, labels, title, os.path.join(results_location, filename))
-
-
-def visualize_scatters(dataframe):
-    """
-    """
-
-    # Synergy and Nudge Impact
-
-    for i in range(0, len(samples)):
-        for j in range(0, samples[i]["network_size"]):
-            x_values = []
-            colors = []
-            labels = []
-            title = "Synergy vs. Nudge impact with motif size %s and %s-valued logic" % \
-                    (samples[i]["network_size"], samples[i]["logic_size"])
-            axes_labels = ["Nudge impact", "Synergy"]
-            # the random tables
-            for sample in samples[i]["random_tables"]:
-                if len(sample[2]) > 0:
-                    if sample[1] is not None and sample[2][j][1] is not None:
-                        x_values.append([sample[2][j][1], sample[1]])
-                        colors.append("red")
-                        labels.append("Random transition table")
-
-            # the bio table
-            for sample in samples[i]["bio_tables"]:
-                if len(sample[2]) > 0:
-                    if sample[1] is not None and sample[2][j][1] is not None:
-                        x_values.append([sample[2][j][1], sample[1]])
-                        colors.append("blue")
-                        labels.append("Biological transition table")
-
-            x_values = np.array(x_values)
-
-            results_location = "../../data/n=%d_l=%d_e=%f_%s_%s_nosamples=%d" % (samples[i]["network_size"], samples[i]["logic_size"], samples[i]["nudge_size"], nudge_method, synergy_measure, len(samples[i]))
-            filename = "scatter2D_synergy_resilience.pdf"
-            visualize.plot_scatter(x_values, colors, labels, title, os.path.join(results_location, filename), axes_labels)
-
-    # Synergy and Memory
-
-    for i in range(0, len(samples)):
-        x_values = []
-        colors = []
-        labels = []
-        title = "Synergy vs. Memory with motif size %s and %s-valued logic, "\
-                "%s genes targeted, %s nudge size" % \
-                (samples[i]["network_size"], samples[i]["logic_size"], j+1, samples[i]["nudge_size"])
-        axes_labels = ["Memory", "Synergy"]
-        # the random tables
-        for sample in samples[i]["random_tables"]:
-            if len(sample[2]) > 0:
-                if sample[1] is not None and sample[3] is not None:
-                    x_values.append([sample[3], sample[1]])
-                    colors.append("red")
-                    labels.append("Random transition table")
-
-        # the bio table
-        for sample in samples[i]["bio_tables"]:
-            if len(sample[2]) > 0:
-                if sample[1] is not None and sample[3] is not None:
-                    x_values.append([sample[3], sample[1]])
-                    colors.append("blue")
-                    labels.append("Biological transition table")
-
-        x_values = np.array(x_values)
-
-        results_location = "../../data/n=%d_l=%d_e=%f_%s_%s_nosamples=%d" % (samples[i]["network_size"], samples[i]["logic_size"], samples[i]["nudge_size"], nudge_method, synergy_measure, len(samples[i]))
-        filename = "scatter2D_synergy_memory.pdf"
-        visualize.plot_scatter(x_values, colors, labels, title, os.path.join(results_location, filename), axes_labels)
-
-    # Memory and Nudge Impact
-
-    for i in range(0, len(samples)):
-        for j in range(0, samples[i]["network_size"]):
-            x_values = []
-            colors = []
-            labels = []
-            title = "Memory vs. Nudge impact with motif size %s and %s-valued logic, "\
-                    "%s genes targeted, %s nudge size" % \
-                    (samples[i]["network_size"], samples[i]["logic_size"], j+1, samples[i]["nudge_size"])
-            axes_labels = ["Nudge impact", "Memory"]
-            # the random tables
-            for sample in samples[i]["random_tables"]:
-                if len(sample[2]) > 0:
-                    if sample[3] is not None and sample[2][j][1] is not None:
-                        x_values.append([sample[2][j][1], sample[3]])
-                        colors.append("red")
-                        labels.append("Random transition table")
-
-            # the bio table
-            for sample in samples[i]["bio_tables"]:
-                if len(sample[2]) > 0:
-                    if sample[3] is not None and sample[2][j][1] is not None:
-                        x_values.append([sample[2][j][1], sample[3]])
-                        colors.append("blue")
-                        labels.append("Biological transition table")
-                
-            x_values = np.array(x_values)
-            
-            results_location = "../../data/n=%d_l=%d_e=%f_%s_%s_nosamples=%d" % (samples[i]["network_size"], samples[i]["logic_size"], samples[i]["nudge_size"], nudge_method, synergy_measure, len(samples[i]))
-            filename = "scatter2D_memory_resilience.pdf"
-            visualize.plot_scatter(x_values, colors, labels, title, os.path.join(results_location, filename), axes_labels)
-
-    # 3D scatter
-
-    for i in range(0, len(samples)):       
-        for j in range(0, samples[i]["network_size"]):
-            x_values = []
-            colors = []
-            labels = []
-            title = "Synergy vs. Nudge impact with motif size %s and %s-valued logic, "\
-                    "%s genes targeted, %s nudge size" % \
-                    (samples[i]["network_size"], samples[i]["logic_size"], j+1, samples[i]["nudge_size"])
-            axes_labels = ["Nudge impact", "Synergy", "Memory"]
-            # the random tables
-            for sample in samples[i]["random_tables"]:
-                if len(sample[2]) > 0:
-                    if sample[1] is not None and sample[2][j][1] is not None:
-                        x_values.append([sample[2][j][1], sample[1], sample[3]])
-                        colors.append("red")
-                        labels.append("Random transition table")
-
-            # the bio table
-            for sample in samples[i]["bio_tables"]:
-                if len(sample[2]) > 0:
-                    if sample[1] is not None and sample[2][j][1] is not None:
-                        x_values.append([sample[2][j][1], sample[1], sample[3]])
-                        colors.append("blue")
-                        labels.append("Biological transition table")
-                
-            x_values = np.array(x_values)
-            
-            results_location = "../../data/n=%d_l=%d_e=%f_%s_%s_nosamples=%d" % (samples[i]["network_size"], samples[i]["logic_size"], samples[i]["nudge_size"], nudge_method, synergy_measure, len(samples[i]))
-            filename = "scatter3D_memory_synergy_resilience.pdf"
-            
-            visualize.plot_scatter_3d(x_values, colors, labels, title, os.path.join(results_location, filename), axes_labels)
-
-
-
-def visualize_profile(dataframe):
-    """
-    """
-
-    
-def visualize_memory(dataframe):
-    """
-    """
-
-    
-def test_synergy(dataframe):
-    """
-    """
-
-    
-def test_memory(dataframe):
-    """
-    """
-
-    
-def test_resilience(dataframe):
-    """
-    """
-
+# set folders
+data_location = "../../data_pandas"
+archive_location = "../../archive_pandas"
+log_location = "../../log"
     
 def loop_impacts(network_size, nudge_size, motif):
     # find the memory
@@ -255,6 +54,7 @@ def loop_impacts(network_size, nudge_size, motif):
             impacts.append(impact_tuple)
     
     return synergy, impacts, memory
+
 
 def draw_sample(sample_size, network_size, logic_size, nudge_size):
     # create dataframe
@@ -297,7 +97,6 @@ def draw_sample(sample_size, network_size, logic_size, nudge_size):
         df_row.append(nudge_size)
         df_row.append("random")
         df_row.append(motif)
-        motif.set_transition_table()
 
         # compute outputs of experiments
         synergy, impacts, memory = loop_impacts(network_size, nudge_size, motif)
@@ -313,10 +112,6 @@ def draw_sample(sample_size, network_size, logic_size, nudge_size):
     return dataframe, samples_grn, samples_random
 
 def main():
-    # set folders
-    data_location = "../../data"
-    log_location = "../../log"
-
     # logger
     mylogger = logging.getLogger('mylogger')
     handler1 = logging.FileHandler(filename=os.path.join(log_location, 'experiments_%s.log' % strftime("%Y-%m-%d %H:%M:%S", gmtime())), mode='w')
@@ -324,9 +119,15 @@ def main():
     handler1.setFormatter(logging.Formatter('%(asctime)s [%(levelname)s] %(message)s'))
     mylogger.addHandler(handler1)
 
-    directory = "../../data"
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
+    if not os.path.isdir(data_location):
+        os.mkdir(data_location)
+    else:
+        if not os.path.isdir(archive_location):
+            os.mkdir(archive_location)
+        archive_folder = os.path.join(archive_location, str(int(time.time())))
+        shutil.copytree(data_location, archive_folder)
+        shutil.rmtree(data_location)
+        os.mkdir(data_location)
 
     # draw a few completely random samples, with different parameters
     for network_size in network_sizes:
@@ -337,9 +138,9 @@ def main():
                 dataframe, samples_grn, samples_random = draw_sample(sample_size, network_size, logic_size, nudge_size)
                 
                 # save the data for future use/reruns
-                name_df = "experiment_k=%d_l=%d_e=%f_%s_%s_n=%d_df.pkl" % (network_size, logic_size, nudge_size, nudge_method, synergy_measure, sample_size)
-                name_grn = "samples_grn_k=%d_l=%d_n=%d.pkl" % (network_size, logic_size, sample_size)
-                name_random = "samples_random_k=%d_l=%d_n=%d.pkl" % (network_size, logic_size, sample_size)
+                name_df = "experiment_k=%d_l=%d_e=%f_n=%d_df.pkl" % (network_size, logic_size, nudge_size, sample_size * 2)
+                name_grn = "samples_grn_k=%d_l=%d_n=%d.pkl" % (network_size, logic_size, sample_size * 2)
+                name_random = "samples_random_k=%d_l=%d_n=%d.pkl" % (network_size, logic_size, sample_size * 2)
                 with open(os.path.join(data_location, name_df), 'wb') as output:
                     pickle.dump(dataframe, output, pickle.HIGHEST_PROTOCOL)
                 with open(os.path.join(data_location, name_grn), 'wb') as output:
@@ -352,19 +153,16 @@ def main():
                 mylogger.debug("sampled %d motifs" % sample_size)
                 mylogger.debug("sample took %d seconds" % (end - start))
 
-    # load data back, append the dataframes
-    dataframe = pd.DataFrame(columns=["system_size", "logic_size", "nudge_size", "type", "motif", "synergy", "memory", "impacts"])
-    for root, dirnames, filenames in os.walk(data_location):
-        for filename in filenames:
-            if filename.endswith('df.pkl'):
-                with open(os.path.join(root, filename), 'rb') as input:
-                    dataframe.append(pickle.load(input))
+    # save the experiment parameters
+    parameters = {}
+    parameters["synergy_measure"] = synergy_measure
+    parameters["nudge_method"] = nudge_method
+    parameters["sample_size"] = sample_size * 2 # because we take two samples
+    parameters["network_sizes"] = network_sizes
+    parameters["logic_sizes"] = logic_sizes
+    parameters["nudge_sizes"] = nudge_sizes
+    with open(os.path.join(data_location, "parameters.pkl"), 'wb') as output:
+        pickle.dump(dataframe, output, pickle.HIGHEST_PROTOCOL)
 
-    # now let's get rolling and do our tests!
-    visualize_TSNE(dataframe)
-    visualize_scatters(dataframe)
-    visualize_profile(dataframe)
-    visualize_memory(dataframe)
-    test_synergy(dataframe)
-    test_memory(dataframe)
-    test_resilience(dataframe)
+if __name__ == '__main__':
+    main()
--- scripts/discrete_nudge/produce_tex.py
+++ scripts/discrete_nudge/produce_tex.py
@@ -7,6 +7,216 @@ import os
 import pandas as pd
 import re
 
+
+
+# set folders
+data_location = "../../data_pandas"
+result_location = "../../result_pandas"
+log_location = "../../log"
+
+def visualize_TSNE(dataframe):
+    """
+    We use t-SNE to visualize our sample. What we see makes perfect sense: the biological motifs seem to be a subset of the sample space. In higher valued logic systems, the space becomes larger, making the sample size insufficient. As t-SNE also does clustering, it then starts to appear that we have two seperate sample, but this is simply the separation from the very similar biological motifs from the rest.
+    """
+    # always start by finding all experiments
+    experiments_dataframe = dataframe[["system_size", "logic_size", "nudge_size"]]
+    experiments = experiments_dataframe.drop_duplicates().values.tolist()
+    # loop over the experiments
+    for experiment in experiments:
+        # fill the variables
+        system_size = experiment[0]
+        logic_size = experiment[1]
+        nudge_size = experiment[2]
+
+        # get the wanted data
+        a = dataframe["logic_size"] == logic_size
+        b = dataframe["system_size"] == system_size
+        c = dataframe["nudge_size"] == nudge_size
+        selected_dataframe = dataframe[a & b & c]
+        samples = selected_dataframe[["type", "motif"]].values.tolist()
+
+        # now the real specific deal
+        # x_train should be an array of N vectors of length M
+        # where N is the sample size and M is the length of the combined new state of the transition table
+        # we combine both the random and bio samples, but give them different colors
+        x_train = []
+        y_color = []
+        labels = []
+
+        for sample in samples:
+            table = sample[1]
+            sample_vector = []
+            for row in table.transition_table:
+                sample_vector.extend(row[(len(row)/2):])
+            x_train.append(sample_vector)
+            if sample[0] == 'random':
+                y_color.append("red")
+                labels.append("Random transition table")
+            elif sample[0] == 'GRN':
+                y_color.append("blue")
+                labels.append("Biological transition table")
+
+        # run TSNE
+        x_train = np.array(x_train)
+        title = "Two-dimensional embedding of transition tables with motif size %s and %s-valued logic" % (system_size, logic_size)
+        x_train_embedded = TSNE(n_components=2, perplexity=10, early_exaggeration=1, verbose=0).fit_transform(x_train)
+        
+        # save the results
+        foldername = "n=%d_l=%d_e=%f_%s_%s_nosamples=%d" % (system_size, logic_size, nudge_size, nudge_method, synergy_measure_name, len(samples))
+        folderpath = os.path.join(result_location, foldername)
+        if not os.path.isdir(folderpath):
+            os.mkdir(folderpath)
+        filename = "tsne2D.pdf" 
+        visualize.plot_scatter(x_train_embedded, y_color, labels, title, os.path.join(folderpath, filename))
+
+
+def visualize_scatters(dataframe):
+    """
+    """
+    # always start by finding all experiments
+    experiments_dataframe = dataframe[["system_size", "logic_size", "nudge_size"]]
+    experiments = experiments_dataframe.drop_duplicates().values.tolist()
+    # loop over the experiments
+    for experiment in experiments:
+        # fill the variables
+        system_size = experiment[0]
+        logic_size = experiment[1]
+        nudge_size = experiment[2]
+
+        # get the wanted data
+        a = dataframe["logic_size"] == logic_size
+        b = dataframe["system_size"] == system_size
+        c = dataframe["nudge_size"] == nudge_size
+        selected_dataframe = dataframe[a & b & c]
+        samples = selected_dataframe[["type", "synergy", "impacts"]].values.tolist()
+
+        for i in range(0, int(system_size)):
+            x_values = []
+            colors = []
+            labels = []
+            title = "Synergy vs. Nudge impact with motif size %s, %s genes targeted, and %s-valued logic" % \
+                    (system_size, i+1, logic_size)
+            axes_labels = ["Nudge impact", "Synergy"]
+           
+            # read the samples
+            for sample in samples:
+                if len(sample[2]) > 0:
+                    if sample[1] is not None and sample[2][i][1] is not None:
+                        x_values.append([sample[2][i][1], sample[1]])
+                        if sample[0] == "random":
+                            colors.append("red")
+                            labels.append("Random transition table")
+                        elif sample[0] == "GRN":
+                            colors.append("blue")
+                            labels.append("Biological transition table")
+
+            x_values = np.array(x_values)
+
+            foldername = "n=%d_l=%d_e=%f_%s_%s_nosamples=%d" % (system_size, logic_size, nudge_size, nudge_method, synergy_measure_name, len(samples))
+            folderpath = os.path.join(result_location, foldername)
+            if not os.path.isdir(folderpath):
+                os.mkdir(folderpath)
+
+            filename = "scatter2D_synergy_resilience_width_%s.pdf" % str(i+1)
+            visualize.plot_scatter(x_values, colors, labels, title, os.path.join(folderpath, filename), axes_labels)
+
+def visualize_profile(dataframe):
+    """
+    """
+    # always start by finding all experiments
+    experiments_dataframe = dataframe[["system_size", "logic_size", "nudge_size"]]
+    experiments = experiments_dataframe.drop_duplicates().values.tolist()
+    # loop over the experiments
+    for experiment in experiments:
+        # fill the variables
+        system_size = experiment[0]
+        logic_size = experiment[1]
+        nudge_size = experiment[2]
+
+        # get the wanted data
+        a = dataframe["logic_size"] == logic_size
+        b = dataframe["system_size"] == system_size
+        c = dataframe["nudge_size"] == nudge_size
+        selected_dataframe = dataframe[a & b & c]
+        samples = selected_dataframe[["type", "motif"]].values.tolist()
+def visualize_memory(dataframe):
+    """
+    """
+    # always start by finding all experiments
+    experiments_dataframe = dataframe[["system_size", "logic_size", "nudge_size"]]
+    experiments = experiments_dataframe.drop_duplicates().values.tolist()
+    # loop over the experiments
+    for experiment in experiments:
+        # fill the variables
+        system_size = experiment[0]
+        logic_size = experiment[1]
+        nudge_size = experiment[2]
+
+        # get the wanted data
+        a = dataframe["logic_size"] == logic_size
+        b = dataframe["system_size"] == system_size
+        c = dataframe["nudge_size"] == nudge_size
+        selected_dataframe = dataframe[a & b & c]
+        samples = selected_dataframe[["type", "motif"]].values.tolist()
+def test_synergy(dataframe):
+    """
+    """
+    # always start by finding all experiments
+    experiments_dataframe = dataframe[["system_size", "logic_size", "nudge_size"]]
+    experiments = experiments_dataframe.drop_duplicates().values.tolist()
+    # loop over the experiments
+    for experiment in experiments:
+        # fill the variables
+        system_size = experiment[0]
+        logic_size = experiment[1]
+        nudge_size = experiment[2]
+
+        # get the wanted data
+        a = dataframe["logic_size"] == logic_size
+        b = dataframe["system_size"] == system_size
+        c = dataframe["nudge_size"] == nudge_size
+        selected_dataframe = dataframe[a & b & c]
+        samples = selected_dataframe[["type", "motif"]].values.tolist()
+def test_memory(dataframe):
+    """
+    """
+    # always start by finding all experiments
+    experiments_dataframe = dataframe[["system_size", "logic_size", "nudge_size"]]
+    experiments = experiments_dataframe.drop_duplicates().values.tolist()
+    # loop over the experiments
+    for experiment in experiments:
+        # fill the variables
+        system_size = experiment[0]
+        logic_size = experiment[1]
+        nudge_size = experiment[2]
+
+        # get the wanted data
+        a = dataframe["logic_size"] == logic_size
+        b = dataframe["system_size"] == system_size
+        c = dataframe["nudge_size"] == nudge_size
+        selected_dataframe = dataframe[a & b & c]
+        samples = selected_dataframe[["type", "motif"]].values.tolist()
+def test_resilience(dataframe):
+    """
+    """
+    # always start by finding all experiments
+    experiments_dataframe = dataframe[["system_size", "logic_size", "nudge_size"]]
+    experiments = experiments_dataframe.drop_duplicates().values.tolist()
+    # loop over the experiments
+    for experiment in experiments:
+        # fill the variables
+        system_size = experiment[0]
+        logic_size = experiment[1]
+        nudge_size = experiment[2]
+
+        # get the wanted data
+        a = dataframe["logic_size"] == logic_size
+        b = dataframe["system_size"] == system_size
+        c = dataframe["nudge_size"] == nudge_size
+        selected_dataframe = dataframe[a & b & c]
+        samples = selected_dataframe[["type", "motif"]].values.tolist()
+
+
 def create_table(df, experiment, samplesize):
     outfile = open(experiment + ".txt", "w")
     outfile.write(r"\begin{figure}[h]" + "\n")
@@ -91,6 +301,29 @@ def create_table(df, experiment, samplesize):
     return 0
 
 def main():
+    if not os.path.isdir(result_location):
+        os.mkdir(result_location)
+
+    # load data back, append the dataframes
+    dataframe = pd.DataFrame(columns=["system_size", "logic_size", "nudge_size", "type", "motif", "synergy", "memory", "impacts"])
+    for root, dirnames, filenames in os.walk(data_location):
+        for filename in filenames:
+            if filename.endswith('df.pkl'):
+                with open(os.path.join(root, filename), 'rb') as input:
+                    print("opening %s" % input)
+                    dataframe_new = pickle.load(input)
+                    dataframe = pd.concat([dataframe, dataframe_new])
+
+    # now let's get rolling and do our tests!
+    visualize_TSNE(dataframe)
+    visualize_scatters(dataframe)
+    #visualize_profile(dataframe)
+    #visualize_memory(dataframe)
+    #test_synergy(dataframe)
+    #test_memory(dataframe)
+    #test_resilience(dataframe)
+
+    # now create our latex tables
     df = pd.DataFrame(columns=["experiment", "system_size", "logic_size", "nudge_size", "p_value", "color"])
 
     loc_counter = 0
