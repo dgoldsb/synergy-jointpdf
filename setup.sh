cf163ef8bbedb14b3181af308024e80543971442
dgoldsb <dgoldsb@live.nl>
2018-02-22 17:51:41 +0100

    Kicked out all the unused pieces of code

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 # Make sure submodules are up to date and ready to be used
 git submodule update --recursive
-touch ./scripts/discrete_nudge/__init__.py
-touch ./scripts/continuous_nudge/NPEET/__init__.py
-touch ./scripts/discrete_nudge/jointpdf_DJ/code/__init__.py
-ln -s ./scripts/discrete_nudge/jointpdf ./scripts/discrete_nudge/jointpdf_DJ/code/jointpdf
-ln -s ./scripts/discrete_nudge/jointpdf_DJ/code ./scripts/discrete_nudge/jointpdf_DJ_code
+touch ./scripts/__init__.py
+touch ./scripts/jointpdf/__init__.py
 
 if [ ! -d "./log" ]; then
     mkdir log
